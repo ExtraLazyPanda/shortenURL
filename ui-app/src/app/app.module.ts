b216c06de7799710aa7bf649762de04f3d720c18
extralazypanda <R0907@ritu>
2020-07-22 00:51:52 +0530

    Restructured

--- ui-app/src/app/app.module.ts
+++ ui-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RedirectComponent } from './redirect/redirect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedirectComponent
   ],
   imports: [
     BrowserModule
